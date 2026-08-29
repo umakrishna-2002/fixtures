8002f4b5d97fd5572cbe85258299305018e07ea6
Uma Krishna Marineni <129099526+umakrishna-2002@users.noreply.github.com>
2026-08-29 11:04:56 +0530

    used the same fixture using workers to run the tests councurrently

---
+++ dashboards.spec.js
@@ -0,0 +1,26 @@
+const { test } = require('./fixtures');
+const { expect } = require('@playwright/test');
+
+test('verfying sales dashboars', async ({ authenticatedPage }) => {
+  await authenitcationPage.goto('dashboards/sales');
+  await expect  (authenticationPage.locator('#sales-chart')).toBeVisible();
+});
+
+test('verfying Marketing Dashboard', async({ authenticatedPage }) => {
+  await authenticationPage.goto('/dashboards/marketing');
+  await expect(authenticationPage.locator('#campaign-stats')).toBeVisible();
+});
+
+test('verfying HR dashboars', async ({ authenticatedPage }) => {
+  await authenitcationPage.goto('dashboards/hr');
+  await expect  (authenticationPage.locator('#employee-count')).toBeVisible();
+});
+
+test('verfying ENgineer dashboars', async ({ authenticatedPage }) => {
+  await authenitcationPage.goto('dashboards/workers');
+  await expect  (authenticationPage.locator('#tickets-solved')).toBeVisible();
+});
+
+/* You can create a single file or multiple file targeting different dashboards. If you have "workers:4"
+set your <b>playwright.config.js</b>, playwright will spin up 4 workers and crush through these tests concurrently.  
+
