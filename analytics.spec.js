2b2a32dcddc11cf09d09a9c287ee27ce4f5c6084
Uma Krishna Marineni <129099526+umakrishna-2002@users.noreply.github.com>
2026-09-07 13:18:34 +0530

    Clarify fixture usefulness in analytics tests
    Updated comments to clarify the usefulness of the fixture.

--- analytics.spec.js
+++ analytics.spec.js
@@ -10,7 +10,7 @@ test('should dispaly analytics', async({ authenticationPage }) => {
 if you 10 different test files that all need to check different parts of the dashboard (analytics.settings, profiles), they can all resue this single fixtures alone.
 - they don't need to copy-paste the login or logout setups.
 - If your login Phase selectors change in the fixture, you only have to fix the them in one place(fixture file) instead of fixing 10 different test files
-
+*/
   
 
      
